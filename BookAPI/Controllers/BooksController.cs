24408711194bb0105a04fc002d940349cfc6bca3
Dana <dana.gh.3399@gmail.com>
2025-04-18 01:47:59 +0300

    ASP.NET Core Server for first Angular BookList app

---
+++ BookAPI/Controllers/BooksController.cs
@@ -0,0 +1,24 @@
+﻿using BookAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookAPI.Controllers
+{
+    // https://localhost:port/api/books
+    [Route("api/[Controller]")] // dynamic route takes the controller name, removing the keyword "controller"
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private Book[] _books = new Book[]
+        {
+            new Book{ Id = 1, Author = "Author One", Title ="Book One"},
+            new Book{ Id = 2, Author = "Author Two", Title ="Book Two"},
+            new Book{ Id = 3, Author = "Author Three", Title ="Book Three"}
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Book>> GetBooks()
+        { 
+            return Ok(_books);
+        }
+    }
+}
---
+++ BookAPI/Program.cs
@@ -0,0 +1,38 @@
+namespace BookAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            
+            // add services
+            builder.Services.AddControllers();
+
+            builder.Services.AddCors(options => 
+            {
+                options.AddPolicy("MyCors", builder =>
+                {
+                    builder
+                    .WithOrigins("http://localhost:4200")
+                    .AllowAnyHeader()
+                    .AllowAnyMethod();
+                });
+            });
+
+            var app = builder.Build();
+
+            app.UseCors("MyCors");
+            // setting the roots ("pages")
+            // add mapping
+            app.MapControllers();
+
+            app.MapGet("/", () =>
+            {
+                return Results.Redirect("/api/books");
+            });
+
+            app.Run();
+        }
+    }
+}
